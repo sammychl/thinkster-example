e88e61f9877257f99ca6a99501382f8513ae6e12
sammychl <sammychl@gmail.com>
2014-05-12 05:35:46 +0000

    complete ch6 but error handling not working

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,27 +2,35 @@
 'use strict';
 
 var app = angular
-  .module('angNewsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'firebase'
-  ])
+    .module('angNewsApp', [
+	'ngCookies',
+	'ngResource',
+	'ngSanitize',
+	'ngRoute',
+	'firebase'
+    ])
     .constant('FIREBASE_URL', 'https://scorching-fire-3130.firebaseio.com/');
 
 app.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'views/posts.html',
-	controller: 'PostsCtrl'
-      })
-      .when('/posts/:postId', {
-        templateUrl: 'views/showpost.html',
-        controller: 'PostViewCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+	.when('/', {
+            templateUrl: 'views/posts.html',
+	    controller: 'PostsCtrl'
+	})
+	.when('/posts/:postId', {
+            templateUrl: 'views/showpost.html',
+            controller: 'PostViewCtrl'
+	})
+	.when('/register',{
+	    templateUrl: 'views/register.html',
+	    controller: 'AuthCtrl'
+	})
+	.when('/login', {
+	    templateUrl: 'views/login.html',
+	    controller: 'AuthCtrl'
+	})
+	.otherwise({
+            redirectTo: '/'
+	});
 });
 
